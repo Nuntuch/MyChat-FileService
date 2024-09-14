9ef680a64aa4fcd7bebf4cdb28afa57dc8e5443e
Nuntuch Thongyoo DT3 <nuntuch.thongyoo@gmail.com>
2024-09-14 20:41:41 +0700

    CREATE Project

---
+++ fileservice/src/main/java/com/nuntuch/mychat/fileservice/FileserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nuntuch.mychat.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileserviceApplication.class, args);
+	}
+
+}
---
+++ fileservice/src/test/java/com/nuntuch/mychat/fileservice/FileserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuntuch.mychat.fileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
